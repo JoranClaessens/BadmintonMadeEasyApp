1429dbd71409af6852295a8bb53f9771536303f1
Joran Claessens <11400919@student.pxl.be>
2018-03-15 18:12:06 +0100

    home view with slides

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
 
 @Component({
   selector: 'bme-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
+  @ViewChild(Slides) slides: Slides;
 
   constructor() { }
 
   ngOnInit() {
+    
   }
 
+  ionViewDidEnter() {
+    this.keepSliding();
+  }
+
+  keepSliding() {
+    setTimeout(() => {
+      if (!this.slides.isEnd()) {
+        this.slides.slideNext(1000);
+      } else {
+        this.slides.slideTo(0, 1000);
+      }
+      this.keepSliding();
+    }, 4000);
+  }
 }
